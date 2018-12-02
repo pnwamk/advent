e74c39df6fb0ebcb8c1cbb8085ba48ab769a3aa0
Andrew Kent <pnwamk@gmail.com>
2018-12-02 09:51:52 -0500

    Day01 2018

---
+++ aoc2018/src/Day01.hs
@@ -0,0 +1,81 @@
+module Day01
+  ( test
+  , run
+  ) where
+
+import           Data.Set as Set (Set)
+import qualified Data.Set as Set hiding (Set)
+import           Data.List
+import           Test.Hspec
+
+
+data Cmd = 
+    Add Int
+  | Sub Int
+  deriving Show
+
+parseWords :: [String] -> [Cmd]
+parseWords [] = []
+parseWords ((sign:num):rst) = 
+  case sign of
+    '+' -> (Add (read num)):(parseWords rst)
+    '-' -> (Sub (read num)):(parseWords rst)
+    _ -> error $ "bad sign: " ++ [sign]
+parseWords (bad:rst) = error $ "bad token: " ++ bad
+
+runCmd :: Cmd -> Int -> Int
+runCmd (Add n) acc = acc + n
+runCmd (Sub n) acc = acc - n
+
+runCmds :: [Cmd] -> Int
+runCmds cmds = foldr runCmd 0 cmds 
+
+runStr :: String -> Int
+runStr input = runCmds $ parseWords $ words input 
+
+
+findDup :: [Int] -> Maybe Int
+findDup ns = go ns Set.empty
+  where go :: [Int] -> Set Int -> Maybe Int
+        go [] _ = Nothing
+        go (x:xs) seen
+          | Set.member x seen = Just x
+          | otherwise         = go xs (Set.insert x seen)
+
+logCmds :: [Cmd] -> [Int]
+logCmds cmds = go cmds 0
+  where go :: [Cmd] -> Int -> [Int]
+        go [] acc = [acc]
+        go (c:cs) acc = acc:(go cs acc')
+          where acc' = runCmd c acc
+
+findDupFreq :: String -> Maybe Int
+findDupFreq input = findDup $ logCmds $ cs
+  where cmds = parseWords $ words input
+        cs = cmds ++ cs
+
+test :: IO ()
+test = hspec $ do
+  describe "Day 01 part 1 (resulting freq)" $ do
+    it "+1, +1, +1" $ do
+      runStr "+1 +1 +1" `shouldBe` 3
+    it "+1, +1, -2" $ do
+      runStr "+1 +1 -2" `shouldBe` 0
+    it "-1, -2, -3" $ do
+      runStr "-1 -2 -3" `shouldBe` -6
+  describe "Day 01 part 2 (first repeated freq)" $ do
+    it "+1, -1" $ do
+      findDupFreq "+1 -1" `shouldBe` (Just 0)
+    it "+3, +3, +4, -2, -4" $ do
+      findDupFreq "+3 +3 +4 -2 -4" `shouldBe` (Just 10)
+    it "-6, +3, +8, +5, -6" $ do
+      findDupFreq "-6 +3 +8 +5 -6" `shouldBe` (Just 5)
+    it "+7, +7, -2, -7, -4" $ do
+      findDupFreq "+7 +7 -2 -7 -4" `shouldBe` (Just 14)
+
+
+run :: IO ()
+run = do
+  input <- readFile "input/Day01.txt"
+  putStrLn $ show $ runStr input
+  putStrLn $ show $ findDupFreq input
---
+++ aoc2018/src/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import qualified Day01 as Day01
+
+main :: IO ()
+main = do
+  putStrLn "Day01"
+  Day01.test
+  Day01.run
